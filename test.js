805218b361f6c7b6a4613f0c42f563c49234fb4e
Christopher Joel <chris@cloudflare.com>
2010-07-25 16:07:35 -0700

    Poop!

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var http = require('http'),
     faye = require('faye');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello Poop\n');
 }).listen(8124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:8124/');
