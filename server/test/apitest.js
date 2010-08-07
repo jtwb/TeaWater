024f23254d3f0e8ea6140c5d500d99204b6a2400
jtwb <jason.benterou+github@gmail.com>
2010-08-06 23:13:47 -0700

    Build tests for latest API version, include async tests

--- server/test/apitest.js
+++ server/test/apitest.js
@@ -5,15 +5,48 @@ var und = function(x) {
 	return typeof x === 'undefined';
 };
 
+var apibase = "http://localhost:8000/v1/resource";
+
 var fayechannel = '/apitest',
-    fayeclient = new Faye.Client('/comet');
+    fayeclient = new Faye.Client('http://localhost:8000/comet');
 
-fayeclient.subscribe(fayechannel, function(){});
+var noop = function(message) {},
+    callback = noop,
+    handle = fayeclient.subscribe(fayechannel, function(message){
+    callback.apply(this, arguments);
+});
 
+/*
+ * Reset the server at the start
+ */
 test("Reset server state", function() {
-	// TODO send a TEST SERVER RESET message
-	// so any test game objects are cleared
-   ok(true, "Server state reset successful");
+   callback = function(message) {
+     ok(true, "Server state reset successful");
+     start();
+     callback = noop;
+   };
+   fayeclient.publish(fayechannel, {command:"reset"});
+   stop();
+});
+
+/*
+ * Join the server
+ */
+test("Join server", function() {
+    $.ajax({
+        type: "PUT",
+        url: apibase + "/meta",
+        data: "username=Jasonator&clientVersion=0.1.1",
+        success: function(message) {
+            ok(true, "Server responded OK to join request");
+            start();
+        },
+        error: function(message) {
+            ok(false, "Server failed in join request");
+            start();
+        }
+    });
+    stop();
 });
 
 test("Create game objects", function() {
@@ -23,7 +56,5 @@ test("Create game objects", function() {
 	ok(true, 'Faye published {ob:"ject"} to ' + fayechannel);
 	// TODO verify entities were created
 });
-
-client.unsubscribe(fayechannel);
 
 })();
