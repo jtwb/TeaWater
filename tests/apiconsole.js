637cbf9b8bac27f47e7af9929a602aa7bff92d69
jtwb <jason.benterou+github@gmail.com>
2010-08-15 02:20:25 -0700

    Add scrolling list of updates

--- tests/apiconsole.js
+++ tests/apiconsole.js
@@ -1,5 +1,7 @@
 
-(function(){
+$(function(){
+
+console.log
 
 var und = function(x) {
 	return typeof x === 'undefined';
@@ -8,27 +10,30 @@ var und = function(x) {
 WorldConsole = function(el) {
 
     var self = this,
-        worldLogEl = $('#world ul'),
-        fayechannel = '/apitest',
+        worldLogEl = $('<ol />').appendTo('#world'),
+        fayechannel = '/world',
         fayeclient = new Faye.Client('http://localhost:8000/comet'),
         onCometPush, subscription;
     console.log(worldLogEl);
 
     onCometPush = function(message) {
-        console.log(JSON.stringify(message));
-        worldLogEl.append('<li>' + message + '</li>');
+        var output = JSON.stringify(message);
+        console.log(output);
+        worldLogEl.append('<li>' + output + '</li>');
     };
 
     subscription = fayeclient.subscribe(fayechannel, onCometPush);
 
-var send = function(){
-    console.log('sending message');
-    fayeclient.publish(fayechannel, {command:"reset"});
-};
-setInterval(send, 4000);
+/*
+    var send = function(){
+        console.log('sending message');
+        fayeclient.publish(fayechannel, {command:"reset"});
+    };
+    setInterval(send, 4000);
+*/
 
 };
 
 new WorldConsole('#world');
 
-})();
+});
